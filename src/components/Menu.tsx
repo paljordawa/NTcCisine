ab2cb9cd5ba3adc22d00ca3f1208140f84c40615
paljordawa <paljordawa@gmail.com>
2026-03-10 13:55:55 +0530

    added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -55,7 +55,7 @@ export default function Menu() {
     const activeSubCategory = activeMainCategory.subCategories.find(s => s.id === activeSubId) || activeMainCategory.subCategories[0];
 
     return (
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 relative">
+        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 relative ">
 
             {/* Cart Floating Button - Top Right */}
             <button
